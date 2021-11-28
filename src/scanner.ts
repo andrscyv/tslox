f6c05cf9019776fb2c72fbbe5cfe7aac7063831d
Andres Cruz <andrscyv@gmail.com>
2021-11-27 18:51:14 -0600

    feat: Add Scanner and token basic functionality

--- src/scanner.ts
+++ src/scanner.ts
@@ -1,11 +1,86 @@
-import { Token } from './token';
+import { Token, TokenType } from './token';
 
 export class Scanner {
-  constructor(input) {
-    console.log('Scanner.constructor', input);
+  currentIdx: number;
+  startIdx: number;
+  line: number;
+  private tokens: Token[];
+
+  constructor(private source: string) {
+    console.log('Scanner.constructor', source);
+    this.currentIdx = 0;
+    this.startIdx = 0;
+    this.line = 1;
+    this.tokens = [];
   }
+
   scanTokens(): Token[] {
-    console.log('Scanner.scanTokens');
-    return [];
+    const tokens: Token[] = [];
+    while (!this.isAtEnd()) {
+      this.startIdx = this.currentIdx;
+      this.scanToken();
+    }
+    tokens.push(new Token(TokenType.EOF, '', null, this.line));
+    return tokens;
+  }
+
+  isAtEnd(): boolean {
+    return this.currentIdx >= this.source.length;
+  }
+
+  advance(): string {
+    this.currentIdx++;
+    return this.source[this.currentIdx - 1];
+  }
+
+  currentLexeme(): string {
+    return this.source.substring(this.startIdx, this.currentIdx + 1);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  addToken(tokenType: TokenType, literal: any = null): void {
+    const lexeme = this.currentLexeme();
+    this.tokens.push(new Token(tokenType, lexeme, literal, this.line));
+  }
+
+  scanToken(): void {
+    const char = this.advance();
+    switch (char) {
+      case '(':
+        this.addToken(TokenType.LEFT_PAREN);
+        break;
+      case ')':
+        this.addToken(TokenType.RIGHT_PAREN);
+        break;
+      case '{':
+        this.addToken(TokenType.LEFT_BRACE);
+        break;
+      case '}':
+        this.addToken(TokenType.LEFT_PAREN);
+        break;
+      case ',':
+        this.addToken(TokenType.LEFT_PAREN);
+        break;
+      case '.':
+        this.addToken(TokenType.LEFT_PAREN);
+        break;
+      case '-':
+        this.addToken(TokenType.LEFT_PAREN);
+        break;
+      case '+':
+        this.addToken(TokenType.LEFT_PAREN);
+        break;
+      case ';':
+        this.addToken(TokenType.LEFT_PAREN);
+        break;
+      case '/':
+        this.addToken(TokenType.LEFT_PAREN);
+        break;
+      case '*':
+        this.addToken(TokenType.LEFT_PAREN);
+        break;
+      default:
+        break;
+    }
   }
 }
--- src/token.ts
+++ src/token.ts
@@ -1,1 +1,62 @@
-export class Token {}
+export enum TokenType {
+  // Single-character tokens.
+  LEFT_PAREN = '(',
+  RIGHT_PAREN = ')',
+  LEFT_BRACE = '{',
+  RIGHT_BRACE = '}',
+  COMMA = ',',
+  DOT = '.',
+  MINUS = '-',
+  PLUS = '+',
+  SEMICOLON = ';',
+  SLASH = '/',
+  STAR = '*',
+
+  // One or two character tokens.
+  BANG = '!',
+  BANG_EQUAL = '!=',
+  EQUAL = '=',
+  EQUAL_EQUAL = '==',
+  GREATER = '>',
+  GREATER_EQUAL = '>=',
+  LESS = '<',
+  LESS_EQUAL = '<=',
+
+  // Literals.
+  IDENTIFIER = 'IDENTIFIER',
+  STRING = 'STRING',
+  NUMBER = 'NUMBER',
+
+  // Keywords.
+  AND = 'AND',
+  CLASS = 'CLASS',
+  ELSE = 'ELSE',
+  FALSE = 'FALSE',
+  FUN = 'FUN',
+  FOR = 'FOR',
+  IF = 'IF',
+  NIL = 'NIL',
+  OR = 'OR',
+  PRINT = 'PRINT',
+  RETURN = 'RETURN',
+  SUPER = 'SUPER',
+  THIS = 'THIS',
+  TRUE = 'TRUE',
+  VAR = 'VAR',
+  WHILE = 'WHILE',
+
+  EOF = 'EOF',
+}
+export class Token {
+  constructor(
+    public type: TokenType,
+    public lexeme: string,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    public literal: any,
+    public line: number,
+  ) {}
+
+  toString(): string {
+    return `Token{type=${this.type}, lexeme='${this.lexeme}', literal=${this.literal}, line=${this.line}}`;
+  }
+}
